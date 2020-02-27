0ec3814defa4b87fd51445a380ccf7944fd71c1d
Joshua Allen <55251797+joshua-oss@users.noreply.github.com>
2020-02-26 21:23:24 -0800

    Add postgres

--- data_access/readers/call_postgres.py
+++ data_access/readers/call_postgres.py
@@ -1,12 +1,12 @@
 import pandas as pd
-from burdock.reader.sql.sqlserver import SqlServerReader
+from burdock.reader.sql.postgres import PostgresReader
 from burdock.sql import PrivateReader, CollectionMetadata
 
 meta = CollectionMetadata.from_file('PUMS_large.yaml')
 
 query = 'SELECT married, COUNT(*) AS n FROM PUMS.PUMS_large GROUP BY married'
 
-reader = SqlServerReader('127.0.0.1', 'PUMS', 'sa')
+reader = PostgresReader('127.0.0.1', 'PUMS', 'postgres')
 private_reader = PrivateReader(reader, meta, 1.0)
 
 private = private_reader.execute_typed(query)
