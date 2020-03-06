b367b3690b6402f750e10699caa966f5e44d15e6
Joshua Allen <55251797+joshua-oss@users.noreply.github.com>
2020-03-06 08:32:05 -0800

    Remove extraneous

--- testing/databases/SqlServer/restore.sql
+++ testing/databases/SqlServer/restore.sql
@@ -1,15 +1,3 @@
-CREATE DATABASE [Devices];
-GO
-
-USE Devices;
-GO
-
-CREATE SCHEMA Telemetry
-	CREATE TABLE Crashes (Region varchar(50), Temperature double precision, Building varchar(20), DeviceID int, Refurbished bit, Crashes int)
-	CREATE TABLE Census (DeviceID int, [OEM] varchar(50), Memory varchar(50), [Disk] bigint )
-	CREATE TABLE Rollouts (DeviceID int, RolloutID int, [StartTrial] datetime, [EndTrial] datetime, [TrialGroup] int);
-
-GO
 
 CREATE DATABASE PUMS;
 GO
@@ -22,18 +10,3 @@ CREATE SCHEMA PUMS
 	CREATE TABLE PUMS_large (PersonID int, state int, puma int, sex int, age int, educ int, income float, latino bit, black bit, asian bit, married bit);
 GO
 
-
-
-CREATE DATABASE [Legacy];
-GO
-
-USE Legacy;
-GO
-
-
-CREATE TABLE Crashes (Region varchar(50), Temperature double precision, Building varchar(20), [DeviceID] int, Refurbished bit, Crashes int);
-GO
-CREATE TABLE Census ([DeviceID] int, [OEM] varchar(50), Memory varchar(50), [Disk] bigint );
-GO
-CREATE TABLE [Rollouts] ([DeviceID] int, RolloutID int, [StartTrial] datetime, [EndTrial] datetime, [Group] int);
-GO
